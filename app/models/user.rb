61022d10e89b27a4ff013bd8761314b415a9664a
Damany Bailey <damanybailey@icloud.com>
2021-09-13 09:17:49 -0500

    sign-up form created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  vaildate :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be valid email"}
+  validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be valid email"}
 end
